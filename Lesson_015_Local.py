9a0ac239d4f9e61b7003c821e47fac34d5dc475b
shrikantkhule100-ux <shrikantkhule100@gmail.com>
2026-02-06 13:51:10 +0530

    Create Lesson_015_Local.py

---
+++ Lesson_015_Local.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+data =  np.array([10,20,30,40,50])
+doubled = data*2
+print("---LENOVO AI STATION TEST --")
+print("Orignal Data :",data)
+print("Doubled Data:", doubled)
+print("Pandas Version Check:", pd.__version__ )
